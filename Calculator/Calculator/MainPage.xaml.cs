de1c88c58f0258d4ecd5ae70839f5b51ba5e8eba
tejvi <tejvi@LAPTOP-S4F89UFD>
2022-02-01 13:28:20 -0500

    Add project files.

---
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            lblResult.IsVisible = false;
+        }
+
+        private void btnAdd_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                int fistNum = Convert.ToInt32(edtFirstNumber.Text);
+                int secondNum = Convert.ToInt32(edtSecondNumber.Text);
+
+                int result = fistNum + secondNum;
+
+                lblResult.IsVisible = true;
+                lblResult.Text = $"{fistNum} + {secondNum} = {result}";
+                lblResult.TextColor = Color.Green;
+            }
+            catch (Exception exception)
+            {
+                lblResult.IsVisible = true;
+                lblResult.TextColor = Color.Red;
+                lblResult.Text = $"Invalid numbers! {exception.ToString()}";
+            }
+        }
+
+        private void btnSubtract_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                int fistNum = Convert.ToInt32(edtFirstNumber.Text);
+                int secondNum = Convert.ToInt32(edtSecondNumber.Text);
+
+                int result = fistNum - secondNum;
+                lblResult.IsVisible = true;
+                lblResult.Text = $"{fistNum} - {secondNum} = {result}";
+                lblResult.TextColor = Color.Green;
+            }
+            catch (Exception exception)
+            {
+                lblResult.IsVisible = true;
+                lblResult.TextColor = Color.Red;
+                lblResult.Text = $"Invalid numbers! {exception.ToString()}";
+            }
+        }
+
+        private void btnMultiply_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                int fistNum = Convert.ToInt32(edtFirstNumber.Text);
+                int secondNum = Convert.ToInt32(edtSecondNumber.Text);
+
+                int result = fistNum * secondNum;
+
+                lblResult.IsVisible = true;
+                lblResult.Text = $"{fistNum} * {secondNum} = {result}";
+                lblResult.TextColor = Color.Green;
+            }
+            catch (Exception exception)
+            {
+                lblResult.IsVisible = true;
+                lblResult.TextColor = Color.Red;
+                lblResult.Text = $"Invalid numbers! {exception.ToString()}";
+            }
+        }
+
+        private void btnDivide_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                int fistNum = Convert.ToInt32(edtFirstNumber.Text);
+                int secondNum = Convert.ToInt32(edtSecondNumber.Text);
+
+                int result = fistNum / secondNum;
+
+                lblResult.IsVisible = true;
+                lblResult.Text = $"{fistNum} / {secondNum} = {result}";
+                lblResult.TextColor = Color.Green;
+            }
+            catch (Exception exception)
+            {
+                lblResult.IsVisible = true;
+                lblResult.TextColor = Color.Red;
+                lblResult.Text = $"Invalid numbers! {exception.ToString()}";
+            }
+        }
+    }
+}
